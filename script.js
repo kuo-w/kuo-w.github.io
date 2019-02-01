0edd77408de258140e7ec40a710240d7d93f3cd8
William Kuo <william.k.kuo@gmail.com>
2019-01-31 23:12:01 -0500

    Simplify

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-// $(document).ready(function() {
-//   $(window).scroll(function() {
-//     $(".skills").css(
-//       "opacity",
-//       1 - $(window).scrollTop() / $(".container").height()
-//     );
-//   });
-// });
+const SECTIONS = ["experience", "projects"];
+
+document.addEventListener("DOMContentLoaded", function() {
+  SECTIONS.forEach(section => {
+    document.getElementById(`btn-${section}`).addEventListener(
+      "click",
+      e => {
+        const section = e.target.id.split("btn-")[1];
+        toggleSectionsVisibility(section);
+      },
+      false
+    );
+  });
+
+  function toggleSectionsVisibility(selectedSection) {
+    const $section = $(`#${selectedSection}`);
+    if ($section.is(":hidden")) {
+      $section.slideDown("slow");
+    } else {
+      $section.slideUp("slow");
+    }
+  }
+});
