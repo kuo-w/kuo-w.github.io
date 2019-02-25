8ddee7edd9f6c95069899425963783ea4ea6ad44
William Kuo <william.k.kuo@gmail.com>
2019-02-24 23:12:37 -0500

    Clean up code and add analytics

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 window.$ = require("jquery");
 
 const SECTIONS = ["about", "experience", "projects"];
+const CONTACTS = ["resume", "github", "linkedin", "email"];
 
-document.addEventListener("DOMContentLoaded", function() {
-  SECTIONS.forEach(section => {
-    document.getElementById(`btn-${section}`).addEventListener(
-      "click",
-      element => {
-        const sectionName = element.target.id.split("btn-")[1];
-        const $section = $(`#${sectionName}`);
-        if ($section.is(":hidden")) {
-          $section.prependTo($('#blocks'));
-        }
-        toggleSectionsVisibility($section);
-      },
-      false
-    );
-  });
-
-  function toggleSectionsVisibility($section) {
-    if ($section.is(":hidden")) {
-      $section.slideDown("slow");
-    } else {
-      $section.slideUp("slow");
-    }
-  }
+$(document).ready(() => {
+  addSectionClickListeners();
+  addContactClickListeners();
 });
+
+function addContactClickListeners() {
+  CONTACTS.forEach(contact => {
+    $(`#contact--${contact}`).click(() => {
+      sendGAEvent(contact);
+      return true;
+    });
+  });
+}
+
+function addSectionClickListeners() {
+  SECTIONS.forEach(section => {
+    $(`#btn-${section}`).click(() => {
+      toggleSectionVisibility(section);
+      sendGAEvent(section);
+    });
+  });
+}
+
+function toggleSectionVisibility(section) {
+  const $section = $(`#${section}`);
+  if ($section.is(":hidden")) {
+    $section.prependTo($("#blocks"));
+    $section.slideDown("slow");
+  } else {
+    $section.slideUp("slow");
+  }
+}
+
+function sendGAEvent(label) {
+  if (!ga) return;
+  ga("send", "event", "click", "buttons", label);
+}
